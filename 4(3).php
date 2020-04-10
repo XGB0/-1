83566feec0dea58940aed881d2af569874013c76
林连坤 <60473628+XGB0@users.noreply.github.com>
2020-04-11 02:41:08 +0800

    4.10

---
+++ 4(3).php
@@ -0,0 +1,53 @@
+<?php
+// $a = ($b = 4)+3;
+// echo $a;
+
+
+
+// // ????
+// $s = "12" ^ "9";
+// var_dump($s);
+
+
+
+// $a = 1;
+// $b = 1.0;
+// var_dump ($a == $b);
+// var_dump ($a === $b);
+
+
+
+// $a = "1";
+// $b = 1 + $a;
+// var_dump($b);
+// $c = (float)$b;
+// var_dump($c);
+
+
+
+// $i = 1;
+// switch( $i ) {
+//     case 1:
+//         echo "i = 1";
+//     case 2:
+//         echo "i = 2";
+// }
+
+
+
+// // foreach 用来遍历数组
+// $arr1 = array(1=>"a", 2=>"b", 3=>"c");
+// foreach ($arr1 as $key => $value){
+//     print($key." => ".$value);
+//     print("\n");
+// }
+
+
+
+
+
+
+
+
+
+?>
